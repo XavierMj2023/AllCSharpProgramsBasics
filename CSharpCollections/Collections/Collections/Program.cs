ca8e6c8043d6de731318032c025b38106fda1880
ManjDesp <mddes002@gmail.com>
2023-11-19 20:11:09 +0530

    adding c sharp collection - arraylist and list

--- CSharpCollections/Collections/Collections/Program.cs
+++ CSharpCollections/Collections/Collections/Program.cs
@@ -1,7 +1,30 @@
-﻿public class Program
+﻿using System.Collections;
+using Collections.Chap01ArrayList;
+using Collections.List;
+
+public class Program
 {
 	public static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		while (true) 
+		{
+			Console.WriteLine("Enter the Option to execute operation");
+			string option = Console.ReadLine();
+			if (option.ToLower() == "done")
+			{
+				break;
+			}
+			switch (option.ToLower())
+			{
+				case "arraylist":
+					Chap01ArrayList.ArrayListswitch();
+					break;
+
+				case "list":
+					ListOps listOps = new ListOps();
+					listOps.ListOpsSwitch();
+					break;
+			}
+		}
 	}
 }
