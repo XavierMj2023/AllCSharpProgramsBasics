bf648528416b0300bffd006a5e30bcd6de1c7162
ManjDesp <mddes002@gmail.com>
2023-11-19 21:05:52 +0530

    merging bridgelabz and collection from allcsharp basics

---
+++ CSharpCollections/Collections/Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Collections.Chap01ArrayList;
+using Collections.List;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		while (true) 
+		{
+			Console.WriteLine("Enter the Option to execute operation");
+			string option = Console.ReadLine();
+			if (option.ToLower() == "done")
+			{
+				break;
+			}
+			switch (option.ToLower())
+			{
+				case "arraylist":
+					Chap01ArrayList.ArrayListswitch();
+					break;
+
+				case "list":
+					ListOps listOps = new ListOps();
+					listOps.ListOpsSwitch();
+					break;
+			}
+		}
+	}
+}
