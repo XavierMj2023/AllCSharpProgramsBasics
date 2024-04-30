e3633e34db4357fa1233702a7dec7e6dc0fac3e3
ManjDesp <mddes002@gmail.com>
2024-04-30 07:50:56 +0530

    deleted mssql closed project

--- MSSQLCheatSheet/MSSQLBasics/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MSSQLBasics.MvcApplication" Language="C#" %>
--- MSSQLCheatSheet/MSSQLPrograms/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MSSQLPrograms.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
