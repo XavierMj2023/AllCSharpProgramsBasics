0251146ffde0b2c673768c2f5fd91de7754f0f7b
ManjDesp <mddes002@gmail.com>
2024-05-22 16:28:07 +0530

    adding empty project not using

---
+++ AllCSharpBasicsPractiseForIndecomm/AllCSharpBasicsPractiseForIndecomm/BasicProgramForInterviews/BasicPrograms/FibonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllCSharpBasicsPractiseForIndecomm.BasicProgramForInterviews.BasicPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void PrintFibonacciSeries(int n)
+        {
+            int first = 0, second = 1, next;
+
+            if (n < 1)
+                return;
+
+            Console.Write(first + " ");
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write(second + " ");
+                next = first + second;
+                first = second;
+                second = next;
+            }
+        }
+        public static void FibonacciSeriesProgram()
+        {
+            int n = 10; // Number of terms in the Fibonacci series
+            PrintFibonacciSeries(n);
+        }
+    }
+}
---
+++ AllCSharpBasicsPractiseForIndecomm/AllCSharpBasicsPractiseForIndecomm/Program.cs
@@ -0,0 +1,18 @@
+﻿using AllCSharpBasicsPractiseForIndecomm.BasicProgramForInterviews;
+using AllCSharpBasicsPractiseForIndecomm.Boxing;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		//Boxing
+		BoxingDemo boxDemo = new BoxingDemo();
+		boxDemo.SendMessage();
+
+
+		//Basic Program for Interviews
+		
+		//1 - All Basic Program Repository
+		AllBasicProgramRepository.SendMessage();
+	}
+}
